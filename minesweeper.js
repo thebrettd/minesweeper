43c73164629dd0f987c39f389da839daf9cf5d51
thebrettd <thebrettd@gmail.com>
2013-10-07 11:14:07 -0700

    Compute adjacent bombs really working

--- minesweeper.js
+++ minesweeper.js
@@ -1,48 +1,51 @@
 if (Meteor.isClient) {
 
-  Template.main.gameStarted = function() {
-      return Session.get("inProgress") == "TRUE";
-  };
+    Template.main.gameStarted = function () {
+        return Session.get("inProgress") == "TRUE";
+    };
 
-  Template.menu.events({
-    'click input' : function () {
-      Session.set("inProgress", "TRUE");
-      Session.set("currentBoard", newBoard());
-    }
-  });
-
-    Template.board.events({
-        'click input.menu' : function () {
-            Session.set("inProgress", "FALSE");
-        },
-        'click input.cheat' : function () {
-            console.log("Cheaters never win");
-        },
-        'click input.validate' : function () {
-            console.log("Did I win?");
+    Template.menu.events({
+        'click input': function () {
+            Session.set("inProgress", "TRUE");
+            Session.set("gameOver", "FALSE");
+            Session.set("numClicked", 0);
+            Session.set("currentBoard", newBoard());
         }
     });
 
-    Template.board.clicked = function(i){
-        var board = Session.get("currentBoard");
-        return board[i] == 'X';
-    };
+    Template.board.events({
+        'click input.menu': function () {
+            Session.set("inProgress", "FALSE");
+        },
+        'click input.cheat': function () {
+            tempRevealBombs();
+        },
+        'click input.validate': function () {
+            if (Session.get("numClicked") == 54){
+                window.alert("You win!");
+            }
+        }
+    });
 
-    Template.board.square = function(i){
-        var board = Session.get("currentBoard");
-        if (board[i] == 'X')
-            return computeNumBombs(i);
+    //Compute the value to display in a square
+    Template.board.square = function (squareNum) {
+        var board = getCurrentBoard();
+        if (board[squareNum] == 'X')
+            return computeNumBombs(squareNum);
+        else if (board[squareNum] == 'B' && Session.equals("gameOver","TRUE"))
+            return 'B';
         else
             return ' ';
     };
 
-    function computeNumBombs(i){
+    //Return the number of bombs which are touching cell i
+    function computeNumBombs(i) {
         var adjacentBombCount = 0;
         var adjacentCellsList = computeAdjacentCellsList(i);
-        var board = Session.get("currentBoard");
+        var board = getCurrentBoard();
 
-        for(var j=0;j<adjacentCellsList.length;j++){
-            if (board[adjacentCellsList[j]] == "B"){
+        for (var j = 0; j < adjacentCellsList.length; j++) {
+            if (board[adjacentCellsList[j]] == "B") {
                 adjacentBombCount++;
             }
         }
@@ -50,60 +53,94 @@ if (Meteor.isClient) {
         return adjacentBombCount;
     }
 
-    function computeAdjacentCellsList(i){
+    //Return a list of all of the cell numbers which are adjacent to i
+    function computeAdjacentCellsList(i) {
         var adjacentCellsList = [];
-        if(i-9 >= 0){
-            adjacentCellsList.push(i-9);
+        if (i - 9 >= 0 && !(i % 8 == 0)) {
+            adjacentCellsList.push(i - 9);
         }
-        if(i-8 >= 0){
-            adjacentCellsList.push(i-8);
+        if ((i - 8 >= 0)) {
+            adjacentCellsList.push(i - 8);
         }
-        if(i-7 >= 0){
-            adjacentCellsList.push(i-7);
+        if ((i - 7 >= 0) && !(((i+1) % 8) == 0)) {
+            adjacentCellsList.push(i - 7);
         }
-        if(i-1 >= 0){
-            adjacentCellsList.push(i-1);
+        if ((i - 1 >= 0) && !(i % 8 == 0)) {
+            adjacentCellsList.push(i - 1);
         }
-        if(i+1 < 64){
-            adjacentCellsList.push(i+1);
+        if ((i + 1 < 64) && !(((i+1) % 8) == 0)) {
+            adjacentCellsList.push(i + 1);
         }
-        if(i+7 < 64){
-            adjacentCellsList.push(i+7);
+        if (i + 7 < 64 && !(i % 8 == 0)) {
+            adjacentCellsList.push(i + 7);
         }
-        if(i+8 < 64){
-            adjacentCellsList.push(i+8);
+        if (i + 8 < 64) {
+            adjacentCellsList.push(i + 8);
         }
-        if(i+9 < 64){
-            adjacentCellsList.push(i+9);
+        if (i + 9 < 64 && !(((i+1) % 8) == 0)) {
+            adjacentCellsList.push(i + 9);
         }
         return adjacentCellsList;
     }
 
+    //"Click" all cells adjacent to squareNum which have not been clicked.
+    //A click is indicated by placing an "X" in that cell in the board array.
+    //If a cell is touching 0 bombs, also click all of its adjacent non-clicked cells.
+    function clickAllAdjacent(squareNum) {
+        var board = getCurrentBoard();
+        var adjacentCellsList = computeAdjacentCellsList(squareNum);
+        var clicked = 0;
+        for (var j = 0; j < adjacentCellsList.length; j++) {
+            if(board[adjacentCellsList[j]] != 'X'){ //Do not "click" cells which have already been clicked..
+                board[adjacentCellsList[j]] = 'X';
+                Session.set("numClicked", Session.get("numClicked") + 1);
+                updateBoard(board);
+                if (computeNumBombs(adjacentCellsList[j]) == 0){
+                    clickAllAdjacent(adjacentCellsList[j]);
+                }
+            }
+        }
+    }
+
     Template.board.events({
         'click .square': function (evt) {
-            var board = Session.get("currentBoard");
-            var squareNum = evt.currentTarget.className.split(" ")[1];
-            console.log("squareNum: " + squareNum);
+            if (Session.equals("gameOver","TRUE")) { //Do not allow any more clicking once a bomb has been clicked
+                window.alert("Sorry, game over. Please return to Menu.");
+            } else {
+                var board = getCurrentBoard();
+                var squareNum = evt.currentTarget.className.split(" ")[1];
 
-            if (board[squareNum] == "B"){
-                Session.set("gameOver", "TRUE");
-            }else{
-                board[squareNum] = 'X';
-
+                if (board[squareNum] == "B") {
+                    window.alert("You clicked on a bomb, you lose :(");
+                    Session.set("gameOver", "TRUE");
+                } else {
+                    board[squareNum] = 'X'; //Mark the cell as clicked.
+                    Session.set("numClicked", Session.get("numClicked") + 1);
+                    if (computeNumBombs(squareNum) == 0) {
+                        clickAllAdjacent(squareNum);
+                    }
+                }
+                //Update the board
+                updateBoard(board);
             }
-            //Update the board
-            Session.set("currentBoard", board);
-
         }
     });
+
+    function getCurrentBoard(){
+        return Session.get("currentBoard");
+    }
+
+    function updateBoard(board){
+        Session.set("currentBoard", board);
+    }
 
     function newBoard() {
         var board = [];
         var bombsPlaced = 0;
 
-        while(bombsPlaced < 10){
-            var possibleLocation = Math.floor(Math.random()*64);
-            if (board[possibleLocation] == null){
+        while (bombsPlaced < 10) {
+            var possibleLocation = Math.floor(Math.random() * 64);
+            if (board[possibleLocation] == null) {
                 board[possibleLocation] = 'B';
                 bombsPlaced++;
             }
